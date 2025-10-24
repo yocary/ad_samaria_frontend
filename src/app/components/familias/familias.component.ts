b7b698ebcfccb89a82e14ee332211af9ceec23c1
yocary <mcoronadod@miumg.edu.gt>
2025-10-23 18:34:05 -0600

    Update familias.component.ts

--- src/app/components/familias/familias.component.ts
+++ src/app/components/familias/familias.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-familias',
   templateUrl: './familias.component.html',
-  styleUrls: ['./familias.component.scss']
+  styleUrls: ['./familias.component.scss'],
 })
 export class FamiliasComponent implements OnInit {
   displayedColumns = ['index', 'name', 'actions'];
@@ -25,33 +25,41 @@ export class FamiliasComponent implements OnInit {
 
   ngOnInit(): void {
     this.cargar();
-    this.search.valueChanges.pipe(debounceTime(250)).subscribe(q => this.cargar(q ?? ''));
+    this.search.valueChanges
+      .pipe(debounceTime(250))
+      .subscribe((q) => this.cargar(q ?? ''));
   }
 
   cargar(q?: string) {
-    this.families.listarFamilias(q).subscribe(list => this.data = list);
+    this.families.listarFamilias(q).subscribe((list) => (this.data = list));
   }
 
   openAdd(): void {
-    const ref = this.dialog.open(DialogAddFamilyComponent, { width: '560px', disableClose: true });
-    ref.afterClosed().subscribe(ok => ok && this.cargar(this.search.value ?? ''));
+    const ref = this.dialog.open(DialogAddFamilyComponent, {
+      width: '560px',
+      disableClose: true,
+    });
+    ref
+      .afterClosed()
+      .subscribe((ok) => ok && this.cargar(this.search.value ?? ''));
   }
 
-openEdit(fam: Family): void {
-  console.log('Editando familia:', fam); // 👈 para ver en consola
-  this.dialog.open(DialogEditFamilyComponent, {
-    width: '1000px',
-    data: { family: fam },
-    disableClose: true
-  });
-}
+  openEdit(fam: Family): void {
+    console.log('Editando familia:', fam); // 👈 para ver en consola
+    this.dialog.open(DialogEditFamilyComponent, {
+      width: '1000px',
+      data: { family: fam },
+      disableClose: true,
+    });
+  }
 
   remove(id: number): void {
-    if (!confirm('¿Eliminar esta familia?')) return;
-    this.families.eliminarFamilia(id).subscribe(() => this.cargar(this.search.value ?? ''));
+    this.families
+      .eliminarFamilia(id)
+      .subscribe(() => this.cargar(this.search.value ?? ''));
   }
 
-    regresar(): void {
+  regresar(): void {
     this.router.navigate(['/miembros/home']);
   }
 }
